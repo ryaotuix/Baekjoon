bdcd893d41722e1a6bb25e4cfcd795631eeca0db
ryaotuix <ryaotuix@naver.com>
2023-03-16 14:54:04 -0400

    trial and error

--- DFS n BFS/dfs.cpp
+++ DFS n BFS/dfs.cpp
@@ -7,25 +7,25 @@
 using namespace std;
 
 typedef struct Node {
-    char name;
-    int dfsNum;             // DFS number
-    int dfsFinNum;          // DFS Finish number
-    Node * adjacent;        // adjacent nodes
+    char name = '0';
+    int dfsNum = 0;             // DFS number
+    int dfsFinNum = 0;          // DFS Finish number
+    vector<Node *> adj;       // adjacent nodes array
     int adjCnt = 0;
 } Node;
 
 
 int getInd(char c);     // ex) Node name 'A' would return an index 1
-void nodeInfo(Node n);    // ex) "Node name 'A' has DFS num : 1 & DFS fin num : 16"
+void nodeInfo(Node * n);    // ex) "Node name 'A' has DFS num : 1 & DFS fin num : 16"
 
 
 class Graph
 {
     public:
         // Fields
-        int numNodes;
-        Node ** nodes;      // node array but each node has to be dynamically allocated
-        bool * visited;     // visited array
+        int numNodes;   
+        vector<Node *> nodes;       // Node vector of this graph
+        bool * visited;             // visited array
 
         // Constructor
         Graph(string filename);
@@ -51,43 +51,76 @@ Graph::Graph(string filename)
 
     int nodeNum, edgeNum;
     f >> nodeNum >> edgeNum;        // read from the file and store information
-    // cout << "Total Node Num : " << nodeNum << " & Total Edge Num : " << edgeNum << endl;
 
-    this->nodes = new Node*[nodeNum + 1];  // dynamically allocate node array's memory <is the array become 000>???
+    for (int i = 0; i < nodeNum; ++i)
+    {
+        Node * tempNode = new Node;
+        tempNode->name = i + 'A';
+        tempNode->dfsNum = 0;
+        tempNode->dfsFinNum = 0;
+        tempNode->adjCnt = 0;
+        
+        this->nodes.push_back(tempNode);
+    }
+
+    // testing nodes vector
+    for (int i = 0; i < this->nodes.size(); ++i)
+    {
+        nodeInfo(this->nodes[i]);
+    }
     
+
     char from, to;
     while(f >> from >> to)
     {
+        cout << from << " " << to << "\n";
         // if this node is not created in node list, 
 
         int fIndex = getInd(from);     // index to use for adj            ex) index of 'A' is 0
-        if (this->nodes[fIndex] == nullptr)
+        cout << fIndex << endl;
+
+        nodeInfo(this->nodes[fIndex]);
+
+        if (this->nodes[fIndex]->name != from)
         {
             // dynamically allocate the node information
             Node * nFrom = new Node; 
-            nFrom->name = from; nFrom->dfsNum = 0; nFrom->dfsFinNum = 0; nFrom->adjacent = new Node[nodeNum]; 
+            nFrom->name = from; nFrom->dfsNum = 0; nFrom->dfsFinNum = 0; nFrom->adjacent = new Node*[nodeNum]; 
 
             // and put it 
             this->nodes[fIndex] = nFrom;
+            cout << "Checkpoint\n";
+
         }
         
         int tIndex = getInd(to);
-        if (this->nodes[tIndex] == nullptr)
+        cout << tIndex << endl;
+    
+        nodeInfo(this->nodes[tIndex]);
+
+        if (this->nodes[tIndex]->name != to)
         {
+            cout << "hmm\n";
             // dynamically allocate the node information
             Node * nTo = new Node; 
-            nTo->name = from; nTo->dfsNum = 0; nTo->dfsFinNum = 0; nTo->adjacent = new Node[nodeNum]; 
+            nTo->name = from; nTo->dfsNum = 0; nTo->dfsFinNum = 0; nTo->adjacent = new Node*[nodeNum]; 
 
             // and put it 
             this->nodes[tIndex] = nTo;
+            cout << "Checkpoint\n";
+
         }
     
 
-        // Then, ADD it to adjacent!!
-
-         
+        // Then, ADD it to adjacent!!!
+        Node * fNode = nodes[fIndex];       // (pointer) Node coming from
+        Node * tNode = nodes[tIndex];       // (pointer) Node going to
+        int curradjInd = fNode->adjCnt;     // in fNode, the index of adjacent array to inert
         
+        fNode->adjacent[curradjInd] = tNode;    // update adjacent array 
+        fNode->adjCnt++;                        // increase adjCnt        
     }
+    
 
     
 }
@@ -104,7 +137,7 @@ void nodeInfo(Node * n)
     else 
     {
         cout << "Adjacent Nodes : {";
-        for (int i = 0; i < n->adjCnt; i++) cout << n->adjacent[i].name << ", ";
+        for (int i = 0; i < n->adjCnt; i++) cout << n->adj[i]->name << ", ";
         cout << "}\n";
     }
 }
@@ -113,9 +146,4 @@ int main()
 {
     string filename = "nodesInput.txt";
     Graph g(filename);
-
-
-    Node * n = new Node{'A', 1, 16,};
-    nodeInfo(n);
-
 }
