bb77f8fe4720980fb3f1581e7f9ce182dd0b34a9
ryaotuix <ryaotuix@naver.com>
2023-03-17 17:20:54 -0400

    now let's make methods that implement dfs

--- DFS n BFS/dfs.cpp
+++ DFS n BFS/dfs.cpp
@@ -13,7 +13,7 @@ typedef struct Node {
     vector<Node *> adj;                                         // adjacent nodes array
 } Node;
 
-typedef pair<char, char> edge;
+typedef pair<Node *, Node *> edge;
 
 
 int getInd(char c);                                             // ex) Node name 'A' would return an index 1
@@ -31,27 +31,27 @@ class Graph
         int numNodes;   
         vector<Node *> nodes;                                   // Node vector of this graph
         vector<bool> visited;                                   // visited array
-        int dfsCNT = 0;                                             // DFS Count for DfsNum and DfsFinNum
-        
-        // extra fields
-        vector<edge> backEdges;
-        vector<edge> crossEdges;
-        vector<edge> forwardEdges;
+        int dfsCNT = 0;                                         // DFS Count for DfsNum and DfsFinNum
+        vector<edge> treeEdges;
 
         // Constructor
         Graph(string filename, bool transpose);
 
         // Method
         void dfs(char startNodeName);                           // traverse the graph once from the given node
-        void dfsALL(char starNodeName);                                          // traverse the entire graph from the beginning       
+        void dfsALL(char starNodeName);                         // traverse the entire graph from the beginning       
         void graphInfo();                                       // print graph Info 
+        Node * getNode(char nodeName);                          // get node 
 
         // extra methods
-        bool isBackEdge(const Node * a, const Node * b);
-        bool isCrossEdge(const Node * a, const Node * b);
-        bool isForwardEdge(const Node * a, const Node * b);
+        bool isTreeEdge(const Node * u, const Node * v);
+        bool isBackEdge(const Node * u, const Node * v);
+        bool isCrossEdge(const Node * u, const Node * v);
+        bool isForwardEdge(const Node * u, const Node * v);
+        void edgeInfo(char uName, char vName);
 
         // DO-ABLE
+        void getSTCC();                                         // get Strongly Connected Components in a directed Graph in O(n+m) time 
         bool bipartite();                                       // decide if it is bipartite
         bool isCyclic();                                        // decide if there is a cycle
         bool isEulerCycle();                                    // decide if it is Euler Cycle (can visit all nodes without visiting again)
@@ -108,14 +108,14 @@ Graph::Graph(string filename, bool transpose)
             from = temp;
         }
 
-        cout << from << " " << to << "\n";
+        // cout << from << " " << to << "\n";
 
         int fIndex = getInd(from);                              // index to use for adj    ex) index of 'A' is 0
         int tIndex = getInd(to);
-        cout << fIndex << " " << tIndex << endl;
+        // cout << fIndex << " " << tIndex << endl;
 
-        Node * fNode = this->nodes[fIndex];
-        Node * tNode = this->nodes[tIndex];
+        Node * fNode = getNode(from);
+        Node * tNode = getNode(to);
      
         fNode->adj.push_back(tNode);
     }
@@ -136,7 +136,7 @@ Graph::Graph(string filename, bool transpose)
 void Graph::dfs(char startNodeName)
 {
     int index = getInd(startNodeName);
-    Node * sNode = this->nodes[index];
+    Node * sNode = getNode(startNodeName);
 
     // DFS only when it is not visited
     if (this->visited[index] == false)
@@ -153,7 +153,14 @@ void Graph::dfs(char startNodeName)
         {
             char adjName = sNode->adj[i]->name;                     // each name of adj node
             int adjInd = getInd(adjName);                           // each index of adj node 
-            if (this->visited[adjInd] == false) dfs(adjName);       // recursively call dfs if it is not visited
+            if (this->visited[adjInd] == false)                     // recursively call dfs if it is not visited
+            {
+                edge tempEdge = make_pair(sNode, getNode(adjName));
+                treeEdges.push_back(tempEdge);
+                dfs(adjName);   
+            }    
+            
+             
         }
 
         // iv) this node's DFS Fin Num 
@@ -165,11 +172,87 @@ void Graph::dfs(char startNodeName)
 
 void Graph::dfsALL(char startNodeName) {
     dfs(startNodeName);
-    for (int i = 0; i < visited.size(); ++i)
-    {
-        if (visited[i] == false) dfs(i + 'A');
-    }
+    for (auto curr : this->nodes)
+        this->dfs(curr->name);
 }
+
+// EDGE GOING FROM U TO V (u,v)
+
+bool Graph::isTreeEdge(const Node * u, const Node * v)
+{
+    // This is Linear Search, but we can make it nlogn by sorting and doing a binary search... 그치만 귀찮은걸..
+    for (auto treeEdge : this->treeEdges)
+    {
+        if (treeEdge.first == u && treeEdge.second == v)  return true;
+    }
+
+    return false;
+}
+
+bool Graph::isBackEdge(const Node * u, const Node * v)
+{
+    if (isTreeEdge(u, v)) return false;
+
+    int uDfsNum = u->dfsNum;
+    int uFinNum = u->dfsFinNum;
+    int vDfsNum = v->dfsNum;
+    int vFinNum = v->dfsFinNum;
+
+    if ( (vDfsNum < uDfsNum) && (uDfsNum < uFinNum) && (uFinNum < vFinNum) ) 
+        return true;
+    else 
+        return false;
+}
+
+bool Graph::isCrossEdge(const Node * u, const Node * v)
+{
+    if (isTreeEdge(u, v)) return false;
+
+    int uDfsNum = u->dfsNum;
+    int uFinNum = u->dfsFinNum;
+    int vDfsNum = v->dfsNum;
+    int vFinNum = v->dfsFinNum;
+
+    if ( (vDfsNum < vFinNum) && (vFinNum < uDfsNum) && (uDfsNum < uFinNum))
+        return true;
+    else
+        return false;
+}
+
+bool Graph::isForwardEdge(const Node * u, const Node * v)
+{
+    if (isTreeEdge(u, v)) return false;
+
+    int uDfsNum = u->dfsNum;
+    int uFinNum = u->dfsFinNum;
+    int vDfsNum = v->dfsNum;
+    int vFinNum = v->dfsFinNum;
+
+    if ( (uDfsNum < vDfsNum) && (vDfsNum < vFinNum) && (vFinNum < uFinNum))
+    {
+        return true;
+    }
+    else
+        return false;
+}
+
+void Graph::edgeInfo(char uName, char vName)
+{
+    Node * u = getNode(uName);
+    Node * v = getNode(vName);
+    bool TE = this->isTreeEdge(u, v);
+    bool BE = this->isBackEdge(u, v);
+    bool CE = this->isCrossEdge(u, v);
+    bool FE = this ->isForwardEdge(u, v);
+    
+
+    if (TE) cout << uName << " -> " << vName << " is TreeEdge\n";
+    else if (BE) cout << uName << " -> " << vName << " is BackEdge\n";
+    else if (CE) cout << uName << " -> " << vName << " is CrossEdge\n";
+    else if (FE) cout << uName << " -> " << vName << " is ForwardEdge\n";
+   
+}
+
 
 void Graph::graphInfo()
 {
@@ -178,6 +261,19 @@ void Graph::graphInfo()
         Node * curr = this->nodes[i];
         nodeInfo(curr);
     }
+
+    cout << endl;
+    cout << "Tree Edges:\n";
+    for (auto edge : this->treeEdges)
+    {
+        cout << "{" << edge.first->name << " " << edge.second->name << "}" << endl;
+    }
+}
+
+Node * Graph::getNode(char name)
+{
+    int ind = getInd(name);
+    return this->nodes[ind];
 }
 
 int getInd(char c) {
@@ -201,15 +297,25 @@ void nodeInfo(Node * n)
     }
 }
 
+
 int main()
 {
     string filename = "nodesInput.txt";
     bool transpose = false;
     Graph g(filename, transpose);
 
-    for  (auto curr : g.nodes)
-    {
-        g.dfs(curr->name);
-    }
+    g.dfsALL('A');
     g.graphInfo();
+
+    cout << endl;
+    // Tree Edge example (A, F)
+    g.edgeInfo('A', 'F');
+    // Forward Edge example (A, J)
+    g.edgeInfo('A', 'J');
+    // Cross Edge example (H, G)
+    g.edgeInfo('H', 'G');
+    // Back Edge example (E, F))
+    g.edgeInfo('E', 'F');
+    
+
 }
