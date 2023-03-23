a587b5fb089119f8a76b52e06ac9b2c86143b56c
ryaotuix <ryaotuix@naver.com>
2023-03-23 15:52:41 -0400

    다잘하진 못하지만 열심히

---
+++ DFS n BFS/provingequivalences.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Nodes, Edges;   // 1 <= Nodes <= 20,000     0 <= Edges <= 50,000
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int T; cin >> T;
+
+    while (T--)
+    {
+        cin >> Nodes >> Edges;
+
+    }
+}
--- DFS n BFS/runningMom.cpp
+++ DFS n BFS/runningMom.cpp
@@ -3,51 +3,45 @@
 using namespace std;
 
 // Materials
-unordered_map<string, int> cityToInd;
-unordered_map<int, string> indToCity;
-unordered_map<string, bool> visitedCity;
-bool visited[1000] = {0};
+unordered_map<string, int> cityToInd;       // map city -> index
+unordered_map<int, string> indToCity;       // map index -> city
+// bool visited[10000] = {0};                  // boolean visited array 
+bool inprogress[10000] = {0};               // boolean inprogress array
 
-void printUmap()
-{
-    cout << "Print UMAPS ====================================\n";
-    for (auto i : cityToInd)
-        cout << i.first << " " << i.second << endl;
 
-    cout << endl;
 
-    for (auto i : indToCity)
-        cout << i.first << " " << i.second << endl;
-    
-    cout << endl;
-}
-
+    // dfs traversal using adj Vector, and initial city index
 void dfs(vector<vector<int>> & adjVector, int city, bool & cycle)
 {
-    visited[city] = true;
+    inprogress[city] = true;                            // mark this city as inprogress
 
+    int temp;
     // loop through adjacent nodes
-    for (int adjCity : adjVector[city])
+    for (int adjCity : adjVector[city])                 // for all adjacent cities
     {
-        if (visited[adjCity] == false)
-            dfs(adjVector, adjCity, cycle);
-        else cycle = true; 
+        temp = adjCity;
+        if (inprogress[adjCity] == false)               // if the city is not inprogress
+            dfs(adjVector, adjCity, cycle);             // call it again
+        else 
+        {
+            cycle = true;                               // if the city is inprogress, this graph is cycle
+            return;                                     // terminate
+        }   
     }
+
+    inprogress[city] = false;                           // Once it has gone through all the adjacent list, it is done out of inprogress
+    // visited[city] = true;                               // now it is visited
 }
 
 int main()
 {
-    int n; cin >> n;        // max of n is 5000
+    int n; cin >> n;                                    // max of n is 5000
     
 
     vector<vector<int>> adjVector (2*n, vector<int>()); // vector of vector strings
    
 
-   
-    
-
     int i = 0;
-    // Make a 1 to 1 correspondence to integer (index) and city name
     for (int j = 0; j < n; j++)
     {
         string cityFrom, cityTo; 
@@ -56,34 +50,37 @@ int main()
         // if city coming from is not already in the map,
         if (cityToInd.find(cityFrom) == cityToInd.end())
         {
-            cityToInd[cityFrom] = i;
-            indToCity[i] = cityFrom;
+            cityToInd[cityFrom] = i;                    // make city -> index
+            indToCity[i] = cityFrom;                    // make index -> city
             i++;
         }
 
         // if city going to is not already in 
         if (cityToInd.find(cityTo) == cityToInd.end())
         {
-            cityToInd[cityTo] = i;
-            indToCity[i] = cityTo;
+            cityToInd[cityTo] = i;                      // make city -> index
+            indToCity[i] = cityTo;                      // make index -> city
             i++;
         }
 
-        int indFrom = cityToInd.at(cityFrom);
-        int indTo = cityToInd.at(cityTo);
+        int indFrom = cityToInd.at(cityFrom);           // get index for city coming from
+        int indTo = cityToInd.at(cityTo);               // get index for city going to
 
-        adjVector.at(indFrom).push_back(indTo);
+        adjVector.at(indFrom).push_back(indTo);         // and update adjacency Vector
     }
 
 
     string city;
     while (cin >> city) 
     {
-        memset(visited, 0, sizeof(visited));
-        bool cycle = false;
+        // memset(visited, 0, sizeof(visited));                // make the visited array false
+        memset(inprogress, 0, sizeof(inprogress));          // make the progress array false    
+        bool cycle = false;                                 // default : not cycle
 
-        int indCity = cityToInd.at(city);
-        dfs(adjVector, indCity, cycle);
+        int indCity = cityToInd.at(city);                   // get city -> index    
+        dfs(adjVector, indCity, cycle);                     // dfs to decide if it is a cycle
+
+        // print information
         if (cycle == true) { cout << city << " " << "safe";}
         else cout << city << " "<<  "trapped";
         cout << endl;
