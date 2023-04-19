fa1a1dff1eda5a214b39a95f1fdc7171e3144e15
ryaotuix <ryaotuix@naver.com>
2023-04-19 00:33:26 -0400

    dijkstra implementation

--- cs251hw4.cpp
+++ cs251hw4.cpp
@@ -3,28 +3,14 @@
 #include <limits>
 
 using namespace std;
-#define INF 9999
+#define INF 999999
 
 typedef struct Node {
     char name;
     bool isRed;
 }Node;
 
-void printNode(const Node & n)
-{
-    cout << "Node : " << n.name << " ";
-    if (n.isRed)
-        cout << "RED";
-    else
-        cout << "BLUE";
-    cout << "\n";
-}
-
-// comparator
-bool compareNode(const Node& a, const Node& b)
-{
-    return a.name < b.name;
-}
+typedef pair<int, int> directedEdge; // <index of Node from, weight>
 
 // hashmap to change Node name to node array index
 unordered_map<char, int> toInd = {
@@ -40,11 +26,50 @@ unordered_map<char, int> toInd = {
     {'s', 9},
     {'t', 10}
 };
+unordered_map<int, char> toName = {
+    {0, 'a'},
+    {1, 'b'},
+    {2, 'c'},
+    {3, 'd'},
+    {4, 'e'},
+    {5, 'f'},
+    {6, 'g'},
+    {7, 'h'},
+    {8, 'i'},
+    {9, 's'},
+    {10, 't'}
+};
 
+// PRINT
+void printNode(const Node & n)
+{
+    cout << "Node : " << n.name << " ";
+    if (n.isRed)
+        cout << "RED";
+    else
+        cout << "BLUE";
+    cout << "\n";
+}
+void printDirectedEdge(const directedEdge & de)
+{  
+    cout << "(" << toName[de.first] << ", " << de.second << "), ";
+}
+
+// Comparators
+bool compareNode(const Node& a, const Node& b)
+{
+    return a.name < b.name;
+}
+bool compareDE(const directedEdge & a, const directedEdge & b)
+{
+    return a.second < b.second;
+}
+
+
+/* DFS */
 // Materials
 Node nodes[11];
 int distances[11];
-
 vector<vector<int>> adjList = {
     {3},
     {2},
@@ -58,6 +83,8 @@ vector<vector<int>> adjList = {
     {0, 1, 4},
     {}
 };
+
+
 
 // Delete Invalid Edges
 vector<vector<int>> makeNewAdjList()
@@ -113,10 +140,15 @@ void printDistances()
     }
     cout << "}\n";
 }
-
-
 void bfs(Node & startNode)
 {
+     // distances init INF and init start node as 0
+    for (int i = 0; i < 11; ++i)
+    {
+        distances[i] = INF;
+    }
+    distances[toInd['s']] = 0;
+
     // materials
     queue<Node> Q;
     Q.push(startNode);
@@ -140,10 +172,64 @@ void bfs(Node & startNode)
             Q.push(nodes[ind]);
         }
     }
-
 }
 
 
+
+
+/* DIJKSRA */
+
+vector<vector<directedEdge>> weightedAdjList = {                                    // From
+    {make_pair(toInd['d'], 9)},                                                     // a
+    {make_pair(toInd['c'], 7)},                                                     // b
+    {make_pair(toInd['f'], 15)},                                                    // c
+    {make_pair(toInd['e'], 12)},                                                    // d
+    {make_pair(toInd['g'], 4), make_pair(toInd['i'], 4)},                           // e
+    {make_pair(toInd['h'], 8)},                                                     // f
+    {make_pair(toInd['t'], 6)},                                                     // g
+    {make_pair(toInd['t'], 10)},                                                    // h
+    {make_pair(toInd['t'], 3)},                                                     // i
+    {make_pair(toInd['a'], 5), make_pair(toInd['b'], 6), make_pair(toInd['e'], 27)},// s
+    {}                                                                              // t
+};
+
+// Delete Invalid Edges
+vector<vector<directedEdge>> makeNewWAL(vector<vector<directedEdge>> & WAL)
+{
+    vector<vector<directedEdge>> res;
+    for (int i = 0; i < WAL.size(); i++)
+    {
+        vector<directedEdge> temp;
+        for (int j = 0; j < WAL[i].size(); j++)
+        {
+            // u -> v (if u is blue and v is red it is invalid)
+            if (!nodes[i].isRed && nodes[WAL[i][j].first].isRed)
+            {
+                continue;
+            }
+            temp.push_back(WAL[i][j]);
+        }
+        res.push_back(temp);
+    }
+    return res;
+}
+void printWeighteAdjList(vector<vector<directedEdge>> & WAL)
+{
+    for (int i = 0; i < WAL.size(); ++i)
+    {
+        cout << "Node[" << toName[i] << "]: {";
+        for (directedEdge de : WAL[i])
+        {
+            printDirectedEdge(de);
+        }
+        cout << "}\n";
+    }
+}
+
+// Materials
+// Node nodes[11];      COMMON
+// int distances[11];   COMMON
+priority_queue<directedEdge
 
 int main()
 {
@@ -159,15 +245,7 @@ int main()
     Node s  = {'s', true}; nodes[9] = s;
     Node t  = {'t', false}; nodes[10] = t;
 
-    // distances init INF and init start node as 0
-    for (int i = 0; i < 11; ++i)
-    {
-        distances[i] = INF;
-    }
-    distances[toInd[s.name]] = 0;
-
-
-
+/*
     // START
     printAdjList(adjList);
     vector<vector<int>> newAdjList = makeNewAdjList();      // get rid of invalid edges O(n+m)
@@ -180,6 +258,13 @@ int main()
     cout << distances[indToCheck] << endl;
     // cout << "after bfs\n";
     // printDistances();
+*/
 
+    printWeighteAdjList(weightedAdjList);
+    cout << endl;
+    vector<vector<directedEdge>> newWAL = makeNewWAL(weightedAdjList);  // get rid of invalid edges O(n+m)
+    printWeighteAdjList(newWAL);
+
+    // do Dijkstra updates
 
 }
