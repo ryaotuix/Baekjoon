9d62516aca27fc5ae330cb40096eb379f9c5de03
ryaotuix <ryaotuix@naver.com>
2023-04-19 15:02:01 -0400

    wow

--- cs251hw4.cpp
+++ cs251hw4.cpp
@@ -3,14 +3,14 @@
 #include <limits>
 
 using namespace std;
-#define INF 999999
+#define INF 1e9
 
 typedef struct Node {
     char name;
     bool isRed;
 }Node;
 
-typedef pair<int, int> directedEdge; // <index of Node from, weight>
+typedef pair<int, int> directedEdge; // <weight, ind of Node Towards>
 
 // hashmap to change Node name to node array index
 unordered_map<char, int> toInd = {
@@ -52,7 +52,7 @@ void printNode(const Node & n)
 }
 void printDirectedEdge(const directedEdge & de)
 {  
-    cout << "(" << toName[de.first] << ", " << de.second << "), ";
+    cout << "(" << toName[de.second] << ", " << de.first << "), ";
 }
 
 // Comparators
@@ -60,10 +60,12 @@ bool compareNode(const Node& a, const Node& b)
 {
     return a.name < b.name;
 }
-bool compareDE(const directedEdge & a, const directedEdge & b)
-{
-    return a.second < b.second;
-}
+struct compareDE {                      // make max heap??
+    bool operator()(directedEdge const & a, directedEdge const & b)
+    {
+        return a.first < b.first;
+    }
+};
 
 
 /* DFS */
@@ -140,14 +142,20 @@ void printDistances()
     }
     cout << "}\n";
 }
-void bfs(Node & startNode)
+
+void init(Node & startNode)
 {
-     // distances init INF and init start node as 0
+    // distances init INF and init start node as 0
     for (int i = 0; i < 11; ++i)
     {
         distances[i] = INF;
     }
-    distances[toInd['s']] = 0;
+    distances[toInd[startNode.name]] = 0;
+}
+
+void bfs(Node & startNode, vector<vector<int>> & adj)
+{
+    init(startNode);
 
     // materials
     queue<Node> Q;
@@ -161,10 +169,10 @@ void bfs(Node & startNode)
         int potential = distances[adjInd] + 1;
         // cout << potential << "\n";
         // for each adj node
-        for (int i = 0; i < adjList[adjInd].size(); ++i)
+        for (int i = 0; i < adj[adjInd].size(); ++i)
         {
             // Relax the shortest path if possible
-            int ind = adjList[adjInd][i];
+            int ind = adj[adjInd][i];
             if (potential < distances[ind])
             {
                 distances[ind] = potential;
@@ -180,16 +188,16 @@ void bfs(Node & startNode)
 /* DIJKSRA */
 
 vector<vector<directedEdge>> weightedAdjList = {                                    // From
-    {make_pair(toInd['d'], 9)},                                                     // a
-    {make_pair(toInd['c'], 7)},                                                     // b
-    {make_pair(toInd['f'], 15)},                                                    // c
-    {make_pair(toInd['e'], 12)},                                                    // d
-    {make_pair(toInd['g'], 4), make_pair(toInd['i'], 4)},                           // e
-    {make_pair(toInd['h'], 8)},                                                     // f
-    {make_pair(toInd['t'], 6)},                                                     // g
-    {make_pair(toInd['t'], 10)},                                                    // h
-    {make_pair(toInd['t'], 3)},                                                     // i
-    {make_pair(toInd['a'], 5), make_pair(toInd['b'], 6), make_pair(toInd['e'], 27)},// s
+    {make_pair(9, toInd['d'])},                                                     // a
+    {make_pair(7, toInd['c'])},                                                     // b
+    {make_pair(15, toInd['f'])},                                                    // c
+    {make_pair(12, toInd['e'])},                                                    // d
+    {make_pair(4, toInd['g']), make_pair(4, toInd['i'])},                           // e
+    {make_pair(8, toInd['h'])},                                                     // f
+    {make_pair(6, toInd['t'])},                                                     // g
+    {make_pair(10, toInd['t'])},                                                    // h
+    {make_pair(3, toInd['t'])},                                                     // i
+    {make_pair(5, toInd['a']), make_pair(6, toInd['b']), make_pair(27, toInd['e'])},// s
     {}                                                                              // t
 };
 
@@ -203,7 +211,7 @@ vector<vector<directedEdge>> makeNewWAL(vector<vector<directedEdge>> & WAL)
         for (int j = 0; j < WAL[i].size(); j++)
         {
             // u -> v (if u is blue and v is red it is invalid)
-            if (!nodes[i].isRed && nodes[WAL[i][j].first].isRed)
+            if (!nodes[i].isRed && nodes[WAL[i][j].second].isRed)
             {
                 continue;
             }
@@ -229,7 +237,56 @@ void printWeighteAdjList(vector<vector<directedEdge>> & WAL)
 // Materials
 // Node nodes[11];      COMMON
 // int distances[11];   COMMON
-priority_queue<directedEdge
+
+/* https://hub1234.tistory.com/33 참고*/
+void dijkstra(Node & startNode, vector<vector<directedEdge>> & adj)
+{
+    init(startNode);
+
+    priority_queue<directedEdge> pq;   // pq of directed edges according to weight
+    // DE has <Node To-Ind, weight>
+    
+    pq.push({0, toInd[startNode.name]});   
+    distances[toInd[startNode.name]] = 0;  // shortest path to the start node is 0
+
+    while(!pq.empty())
+    {
+        int currCost = -pq.top().first;
+        int currNode = pq.top().second;      // Actually currNode's index in nodes array
+        pq.pop();
+
+        cout << "Curr Node[" << toName[currNode] << "] : Cost " << currCost << endl;
+
+        // !!!!  Shortest Path already
+        if (distances[currNode] < currCost) continue;
+
+        // adj node of curr node
+        for (int i = 0; i < adj[currNode].size(); ++i)
+        {
+            int newCost = currCost + adj[currNode][i].first;
+            int newNode = adj[currNode][i].second;
+
+           cout << "Adj Node[" << toName[newNode] << "] : Cost " << newCost << endl;
+           
+            // relaxation step
+            if (newCost < distances[newNode])  
+            {
+                int nodeToInd = adj[currNode][i].second;
+                cout << "Node [" << toName[nodeToInd] << "] " << "original weight : " << distances[nodeToInd] << "\n";
+                
+                // Update
+                distances[nodeToInd] = newCost;
+                cout << "Node [" << toName[nodeToInd] << "] " << "new weight : " << distances[nodeToInd] << "\n";
+
+                // push <-newCost, v> when u->v 
+                pq.push(make_pair(-newCost, adj[currNode][i].first));
+                cout << endl;
+            }
+        }
+    }
+    
+}
+
 
 int main()
 {
@@ -253,7 +310,7 @@ int main()
 
 
     // do BFS updates
-    bfs(s);
+    bfs(s, newAdjList);
     int indToCheck = toInd[t.name];
     cout << distances[indToCheck] << endl;
     // cout << "after bfs\n";
@@ -264,7 +321,7 @@ int main()
     cout << endl;
     vector<vector<directedEdge>> newWAL = makeNewWAL(weightedAdjList);  // get rid of invalid edges O(n+m)
     printWeighteAdjList(newWAL);
-
+    cout << endl;
     // do Dijkstra updates
-
+    dijkstra(s, newWAL);
 }
