154ad6733beb1f20c7f98e10377039a53fb737d9
ryaotuix <ryaotuix@naver.com>
2023-06-28 20:13:06 -0500

    .

--- 과외/junghol/탐욕/1183.cpp
+++ 과외/junghol/탐욕/1183.cpp
@@ -1,74 +1,162 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int sum;
+// vector<int> coins(6);
+// vector<int> coinVals = {500, 100, 50, 10, 5, 1};
+// /* {500, 100, 50, 10, 5, 1} */
+// vector<int> ans(6); // answer all filled with 0s
+
+
+// int answer()
+// {
+//     int total = 0;
+
+//     for (int i = coins.size() - 1; i > 0; i--)
+//     {
+//         if (sum == 0)
+//             return total;
+
+//         int currCoinVal = coinVals[i];       // current coin Val
+//         int coinNum = coins[i];             // number of coins
+
+//         int nextCoinVal = coinVals[i-1];     // next coin Val
+
+//         // get optimal coinNum
+//         int j;
+//         for (j = coinNum; j >= 0; j--)
+//         {
+//             // use maximum number of current coin if we can pay the rest with next coin value
+//             if ((sum - j*currCoinVal) % nextCoinVal == 0)
+//                 break;
+//         }
+
+//         sum -= (j*currCoinVal);             // calculate sum 
+//         total += j;                         // increment total
+
+//         ans[i] = j;                         // store how many coins we used
+//     }
+
+//     // The last case when
+//     if (sum != 0)
+//     {
+//         ans[0] = sum / coinVals[0];
+//         total += ans[0];   
+//         return total;
+//     }
+//     else
+//     {
+//         ans[0] = 0;
+//         return total;
+//     }   
+// }
+
+
+// int main()
+// {
+//     // INPUT DONE
+//     cin >> sum;
+//     for (int i = 0; i <coins.size(); i++)
+//     {
+//         cin >> coins[i];
+//     }
+
+
+//     int total = answer();
+//     cout << total << endl;
+//     for (int i = 0; i < ans.size(); i++)
+//     {
+//         cout << ans[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+/*
+Idea:
+    if you can get minimum number of coins, to make total - target,
+    you will get the maximum number of coins
+*/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int sum;
-vector<int> coins(6);
-vector<int> coinVals = {500, 100, 50, 10, 5, 1};
-/* {500, 100, 50, 10, 5, 1} */
-vector<int> ans(6); // answer all filled with 0s
+int target;
+vector<int> coinValues = {500, 100, 50, 10, 5, 1};
+vector<int> coinNum(6);     // initial number of coins
+vector<int> coinSub(6);     // store number of coins to make total - target
 
 
-int answer()
+int getTotal()
 {
-    int total = 0;
+    int total = 0;  // total number of coins
 
-    for (int i = coins.size() - 1; i > 0; i--)
+    int sum = 0;
+    for (int i = 0; i < coinValues.size(); i++)
     {
-        if (sum == 0)
-            return total;
+        sum += coinValues[i] * coinNum[i];
+    }
 
-        int currCoinVal = coinVals[i];       // current coin Val
-        int coinNum = coins[i];             // number of coins
+    int remaining = target - sum;   // target - sum
 
-        int nextCoinVal = coinVals[i-1];     // next coin Val
-
-        // get optimal coinNum
-        int j;
-        for (j = coinNum; j >= 0; j--)
+    int i = 0;
+    while(remaining >= 0 && i < coinValues.size())
+    {
+        if (remaining - coinValues[i] >= 0)
         {
-            // use maximum number of current coin if we can pay the rest with next coin value
-            if ((sum - j*currCoinVal) % nextCoinVal == 0)
-                break;
+            coinSub[i] = coinSub[i] + 1;
+            total++;
+            remaining -= coinValues[i];
         }
-
-        sum -= (j*currCoinVal);             // calculate sum 
-        total += j;                         // increment total
-
-        ans[i] = j;                         // store how many coins we used
+        else
+        {
+            i++;
+        }
     }
 
-    // The last case when
-    if (sum != 0)
-    {
-        ans[0] = sum / coinVals[0];
-        total += ans[0];   
-        return total;
-    }
-    else
-    {
-        ans[0] = 0;
-        return total;
-    }   
+    return total;
 }
-
 
 int main()
 {
-    // INPUT DONE
-    cin >> sum;
-    for (int i = 0; i <coins.size(); i++)
+    cin >> target;
+    for (int i = 0; i < 6; i++)
     {
-        cin >> coins[i];
+        cin >> coinNum[i];
     }
 
+    // INPUT done
 
-    int total = answer();
-    cout << total << endl;
-    for (int i = 0; i < ans.size(); i++)
+
+    cout << getTotal() << endl;
+    for (int i = 0; i < coinSub.size(); i++)
     {
-        cout << ans[i] << " ";
+        cout << coinNum[i] - coinSub[i] << " ";
     }
-
-    return 0;
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
